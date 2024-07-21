dfbee40d4030bc18e6655d1795f82f2d7a49598d
MUHAMMAD EMARA <103325561+muemara1@users.noreply.github.com>
2024-07-21 19:07:19 +0200

    Add solution for babynames.py

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -40,8 +40,43 @@ def extract_names(filename):
   followed by the name-rank strings in alphabetical order.
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
-  # +++your code here+++
-  return
+
+  names = []
+
+
+  with open(filename, 'rt', encoding='utf-8') as f:
+    text = f.read()
+
+
+
+
+  year_match = re.search(r'Popularity\sin\s(\d\d\d\d)', text)
+  if not year_match:
+ 
+    sys.stderr.write('Couldn\'t find the year!\n')
+    sys.exit(1)
+  year = year_match.group(1)
+  names.append(year)
+
+
+  tuples = re.findall(r'<td>(\d+)</td><td>(\w+)</td>\<td>(\w+)</td>', text)
+
+  names_to_rank = {}
+  for rank_tuple in tuples:
+    (rank, boyname, girlname) = rank_tuple  # unpack the tuple into 3 vars
+    if boyname not in names_to_rank:
+      names_to_rank[boyname] = rank
+    if girlname not in names_to_rank:
+      names_to_rank[girlname] = rank
+
+  sorted_names = sorted(names_to_rank.keys())
+
+
+  for name in sorted_names:
+    names.append(name + ' ' + names_to_rank[name])
+
+  return names
+
 
 
 def main():
@@ -63,6 +98,19 @@ def main():
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
+    
+  for filename in args:
+    names = extract_names(filename)
+
+    # Make text out of the whole list
+    text = '\n'.join(names)
+
+    if summary:
+      with open(filename + '.summary', 'w', encoding='utf-8') as outf:
+        outf.write(text + '\n')
+        outf.close()
+    else:
+      print(text)
 
 if __name__ == '__main__':
   main()
