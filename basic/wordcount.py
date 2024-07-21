270bc46322526da59eb06aa40f3397d619e2b5b3
MUHAMMAD EMARA <103325561+muemara1@users.noreply.github.com>
2024-07-21 16:18:55 +0200

    Add comments to functions in wordcount.py

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -68,7 +68,19 @@ def print_top(filename):
         print(f'{word} {count}')
 
 def main():
-  
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
