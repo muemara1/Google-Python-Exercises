b2329a998c7ac5e1ca76050c1593a4b453b094e9
MUHAMMAD EMARA <103325561+muemara1@users.noreply.github.com>
2024-07-21 16:27:35 +0200

    Revert "Add comments to functions"
    This reverts commit bfe2911158e7fcb871bc5a699ab03b3c95c43cf4.

--- basic/list1.py
+++ basic/list1.py
@@ -17,54 +17,32 @@
 
 # A. match_ends
 def match_ends(words):
-    """
-    Given a list of strings, return the count of the number of 
-    strings where the string length is 2 or more and the first 
-    and last characters of the string are the same.
-    """
     count = 0
     for word in words:
-        # Check if the word length is 2 or more and if the first and last characters are the same
         if len(word) >= 2 and word[0] == word[-1]:
             count += 1
     return count
 
 # B. front_x
 def front_x(words):
-    """
-    Given a list of strings, return a list with the strings in 
-    sorted order, except group all the strings that begin with 
-    'x' first.
-    """
     x_list = []
     other_list = []
     
-    # Separate words starting with 'x' and others
     for word in words:
         if word.startswith('x'):
             x_list.append(word)
         else:
             other_list.append(word)
     
-    # Sort both lists and concatenate them with x_list first
     return sorted(x_list) + sorted(other_list)
 
 # C. sort_last
 def sort_last(tuples):
-    """
-    Given a list of non-empty tuples, return a list sorted in 
-    increasing order by the last element in each tuple.
-    """
-    # Sort tuples based on the last element of each tuple
     return sorted(tuples, key=lambda x: x[-1])
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
-    """
-    Compares the got value with the expected value and prints 'OK' if they are the same, 
-    otherwise prints 'X'. Also prints the got and expected values.
-    """
     if got == expected:
         prefix = ' OK '
     else:
@@ -74,14 +52,12 @@ def test(got, expected):
 # Calls the above functions with interesting inputs.
 def main():
     print('match_ends')
-    # Test cases for match_ends function
     test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
     test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
     test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
     print()
     print('front_x')
-    # Test cases for front_x function
     test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
     test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
@@ -91,7 +67,6 @@ def main():
 
     print()
     print('sort_last')
-    # Test cases for sort_last function
     test(sort_last([(1, 3), (3, 2), (2, 1)]),
         [(2, 1), (3, 2), (1, 3)])
     test(sort_last([(2, 3), (1, 2), (3, 1)]),
